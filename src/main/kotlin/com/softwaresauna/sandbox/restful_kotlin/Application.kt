04fa0bdac4759c43c030c33b86fc2cfd2a65fc7d
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-10 14:55:55 +0200

    added missing imports

--- src/main/kotlin/com/softwaresauna/sandbox/restful_kotlin/Application.kt
+++ src/main/kotlin/com/softwaresauna/sandbox/restful_kotlin/Application.kt
@@ -1,5 +1,8 @@
 package com.softwaresauna.sandbox.restful_kotlin
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 @SpringBootApplication
 class Application
 
--- src/main/kotlin/com/softwaresauna/sandbox/restful_kotlin/GreetingController.kt
+++ src/main/kotlin/com/softwaresauna/sandbox/restful_kotlin/GreetingController.kt
@@ -1,5 +1,8 @@
 package com.softwaresauna.sandbox.restful_kotlin
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
